30f0d4c16976b67ee54aa417807484ad25b202c8
Claes Jakobsson <claes.jakobsson@trustly.com>
2018-03-14 14:49:39 +0100

    Make it possible to set ratio and min rows thresholds and check those wether we need to log or not

--- Makefile
+++ Makefile
@@ -5,17 +5,12 @@ REGRESS      = $(patsubst test/sql/%.sql,%,$(TESTS))
 REGRESS_OPTS = --inputdir=test
 MODULES      = $(patsubst %.c,%,$(wildcard *.c))
 PG_CONFIG    ?= pg_config
+PG_CPPFLAGS  = -DEXTNAME=\"$(EXTENSION)\"
 
 all:
 
 release-zip: all
 	git archive --format zip --prefix=pg_planwayoff-$(EXTVERSION)/ --output ./pg_planwayoff-$(EXTVERSION).zip HEAD
-	unzip ./pg_planwayoff-$(EXTVERSION).zip
-	rm ./pg_planwayoff-$(EXTVERSION).zip
-	#sed -i -e "s/__VERSION__/$(EXTVERSION)/g"  ./pg_planwayoff-$(EXTVERSION)/META.json
-	zip -r ./pg_planwayoff-$(EXTVERSION).zip ./pg_planwayoff-$(EXTVERSION)/
-	rm ./pg_planwayoff-$(EXTVERSION) -rf
-
 
 DATA = $(wildcard *--*.sql)
 PGXS := $(shell $(PG_CONFIG) --pgxs)
--- pg_planwayoff.c
+++ pg_planwayoff.c
@@ -5,6 +5,10 @@
 #include "utils/guc.h"
 #include "utils/elog.h"
 
+#ifndef EXTNAME
+#define EXTNAME "pg_badplan"
+#endif
+
 PG_MODULE_MAGIC;
 
 void		_PG_init(void);
@@ -12,8 +16,13 @@ void		_PG_fini(void);
 
 static void pgpwo_ExecutorStart(QueryDesc *queryDesc, int eflags);
 static void pgpwo_ExecutorEnd(QueryDesc *queryDesc);
+static void pgpwo_RecalculateRatio(double val, void *extra);
 
 static bool pgpwo_enabled;
+static double pgpwo_ratio;
+static double pgpwo_ratio_under;
+static double pgpwo_ratio_over;
+static int pgpwo_min_row_threshold;
 
 static ExecutorStart_hook_type prev_ExecutorStart = NULL;
 static ExecutorEnd_hook_type prev_ExecutorEnd = NULL;
@@ -31,7 +40,7 @@ void _PG_init(void) {
 	prev_ExecutorEnd = ExecutorEnd_hook;
 	ExecutorEnd_hook = pgpwo_ExecutorEnd;
 
-	DefineCustomBoolVariable("pg_planwayoff.enabled",
+	DefineCustomBoolVariable(EXTNAME ".enabled",
 							 "Enable / Disable pg_planwayoff",
 							 NULL,
 							 &pgpwo_enabled,
@@ -41,21 +50,61 @@ void _PG_init(void) {
 							 NULL,
 							 NULL,
 							 NULL);
+
+	DefineCustomRealVariable(EXTNAME ".ratio",
+							 "Set ratio",
+							 NULL,
+							 &pgpwo_ratio,
+							 0.2,
+							 0,
+							 1,
+							 PGC_USERSET,
+							 0,
+							 NULL,
+							 pgpwo_RecalculateRatio,
+							 NULL);
+
+	DefineCustomIntVariable(EXTNAME ".min_row_threshold",
+							 "Set minimum row threshold",
+							 NULL,
+							 &pgpwo_min_row_threshold,
+							 1000,
+							 100,
+							 INT_MAX,
+							 PGC_USERSET,
+							 0,
+							 NULL,
+							 NULL,
+							 NULL);
+
 }
 
+/*
+Restore previous hooks on module unload
+*/
 void _PG_fini(void) {
 	ExecutorEnd_hook = prev_ExecutorEnd;
 	ExecutorStart_hook = prev_ExecutorStart;
 }
 
+/* 
+Whenever we change the ratio recalculate the over and under thresholds
+*/
+void pgpwo_RecalculateRatio(double val, void *extra) {
+	pgpwo_ratio_under = val;
+	pgpwo_ratio_over = 1 / val;
+	ereport(LOG, (errmsg_internal(EXTNAME ": setting ratio to %.3f > x > %.3f", pgpwo_ratio_over, pgpwo_ratio_under)));
+}
+
 /*
-Enable instrumentation
+We need to enable instrumentation otherwise the actual rows aren't calculated
 */
 static void pgpwo_ExecutorStart(QueryDesc *queryDesc, int eflags) {
 	if (pgpwo_enabled) {
 		queryDesc->instrument_options |= INSTRUMENT_ROWS;
 	}
 
+	/* Continue running ExecutorStart hooks */
 	if (prev_ExecutorStart) {
 		prev_ExecutorStart(queryDesc, eflags);
 	}
@@ -65,7 +114,7 @@ static void pgpwo_ExecutorStart(QueryDesc *queryDesc, int eflags) {
 }
 
 /*
-Checks if the estimate vs actual is way off and if so log
+Checks if the rows estimate vs rows actual is outside ratio
  */
 static void pgpwo_ExecutorEnd(QueryDesc *queryDesc) {
 	if (pgpwo_enabled) {
@@ -80,10 +129,14 @@ static void pgpwo_ExecutorEnd(QueryDesc *queryDesc) {
 			double actual = queryDesc->planstate->instrument->ntuples / nloops;
 			double ratio = actual/expected;
 
-			ereport(LOG, (errmsg_internal("pgplanwayoff: expected=%.0f actual=%.0f ratio=%.0f", expected, actual, ratio)));
+			if ((ratio <= pgpwo_ratio_under || ratio >= pgpwo_ratio_over) && 
+				(expected > pgpwo_min_row_threshold || actual > pgpwo_min_row_threshold)) {
+				ereport(LOG, (errmsg_internal(EXTNAME ": expected=%.0f actual=%.0f ratio=%.3f (%.3f)", expected, actual, ratio, pgpwo_ratio)));
+			}
 		}
 	}
 	
+	/* Continue running ExecutorEnd hooks */
 	if (prev_ExecutorEnd) {
 		prev_ExecutorEnd(queryDesc);
 	}
