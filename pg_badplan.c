ee092ae35f176f51bf2b362687ee37807cf7001a
Claes Jakobsson <claes.jakobsson@trustly.com>
2018-03-15 17:06:38 +0100

    Make it possible to dump SQL to file if ratio is exceeded

--- pg_badplan.c
+++ pg_badplan.c
@@ -4,6 +4,7 @@
 #include "miscadmin.h"
 #include "utils/guc.h"
 #include "utils/elog.h"
+#include <unistd.h>
 
 #ifndef EXTNAME
 #define EXTNAME "pg_badplan"
@@ -17,12 +18,16 @@ void		_PG_fini(void);
 static void pgpwo_ExecutorStart(QueryDesc *queryDesc, int eflags);
 static void pgpwo_ExecutorEnd(QueryDesc *queryDesc);
 static void pgpwo_RecalculateRatio(double val, void *extra);
+static bool pgpwo_CheckLogdirConf(char **val, void **extra, GucSource src);
 
 static bool pgpwo_enabled;
 static double pgpwo_ratio;
 static double pgpwo_ratio_under;
 static double pgpwo_ratio_over;
 static int pgpwo_min_row_threshold;
+static char *pgpwo_logdir;
+static int pgpwo_min_dump_interval_ms;
+static int64_t pgpwo_last_ts;
 
 static ExecutorStart_hook_type prev_ExecutorStart = NULL;
 static ExecutorEnd_hook_type prev_ExecutorEnd = NULL;
@@ -69,7 +74,31 @@ void _PG_init(void) {
 							 NULL,
 							 &pgpwo_min_row_threshold,
 							 1000,
-							 100,
+							 0,
+							 INT_MAX,
+							 PGC_USERSET,
+							 0,
+							 NULL,
+							 NULL,
+							 NULL);
+
+	DefineCustomStringVariable(EXTNAME ".logdir",
+							"Set log directory",
+							NULL,
+							&pgpwo_logdir,
+							NULL,
+							PGC_USERSET,
+							0,
+							pgpwo_CheckLogdirConf,
+							NULL,
+							NULL);
+
+	DefineCustomIntVariable(EXTNAME ".min_dump_interval_ms",
+							 "Set minimum dump to file interval (in milliseconds)",
+							 NULL,
+							 &pgpwo_min_dump_interval_ms,
+							 60000,
+							 0,
 							 INT_MAX,
 							 PGC_USERSET,
 							 0,
@@ -94,6 +123,35 @@ void pgpwo_RecalculateRatio(double val, void *extra) {
 	pgpwo_ratio_under = val;
 	pgpwo_ratio_over = 1 / val;
 	ereport(LOG, (errmsg_internal(EXTNAME ": setting ratio to %.3f > x > %.3f", pgpwo_ratio_over, pgpwo_ratio_under)));
+}
+
+/*
+Called when setting the logdir. Checks if the dir is available and writeable
+*/
+static bool pgpwo_CheckLogdirConf(char **val, void **extra, GucSource src) {
+	/* Check if dir exists and we can write to it */
+	int r;
+
+	if (val == NULL || *val == NULL) {
+		return TRUE;
+	}
+
+	/* Empty string set to NULL */
+	if (**val == '\0') {
+		*val = NULL;
+		return TRUE;
+	}
+
+	/* Check if we can stat and write to dir */
+	r = access(*val, W_OK);
+	if (r == 0) {
+		return TRUE;
+	}
+	else {
+		ereport(ERROR, (errmsg_internal(EXTNAME ": failed to set logdir. access() returned %d (%s)", r, strerror(r))));
+	}
+
+	return FALSE;
 }
 
 /*
@@ -131,7 +189,34 @@ static void pgpwo_ExecutorEnd(QueryDesc *queryDesc) {
 
 			if ((ratio <= pgpwo_ratio_under || ratio >= pgpwo_ratio_over) && 
 				(expected > pgpwo_min_row_threshold || actual > pgpwo_min_row_threshold)) {
-				ereport(LOG, (errmsg_internal(EXTNAME ": expected=%.0f actual=%.0f ratio=%.3f (%.3f)", expected, actual, ratio, pgpwo_ratio)));
+
+				if (pgpwo_logdir != NULL) {
+					struct timespec tp;
+					clock_gettime(CLOCK_MONOTONIC_RAW, &tp);
+
+					int64_t curr_ms = tp.tv_sec + (tp.tv_nsec / 100000);
+
+
+					if (curr_ms - pgpwo_last_ts > pgpwo_min_dump_interval_ms) {
+						char path[MAXPGPATH];
+						File fd;
+
+						snprintf(path, MAXPGPATH, "%s/" EXTNAME "-%d-%lld.sql", pgpwo_logdir, MyBackendId, curr_ms);
+
+						ereport(LOG, (errmsg_internal(EXTNAME ": writing dump to path %s", path)));
+
+						if ((fd = PathNameOpenFile(path, O_WRONLY | O_CREAT, 0644)) > 0) {
+							FileWrite(fd, (char *) queryDesc->sourceText, strlen(queryDesc->sourceText), 0);
+							FileClose(fd);
+						} else {
+							ereport(LOG, (errmsg_internal(EXTNAME ": failed to open %s because of %s (%d)", path, strerror(fd), fd)));
+						}
+
+						pgpwo_last_ts = curr_ms;
+					}
+				} else {
+					ereport(LOG, (errmsg_internal(EXTNAME ": rows expected/actual ratio %0.3f exceeded for query %s", ratio, queryDesc->sourceText)));
+				}
 			}
 		}
 	}
